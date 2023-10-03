223c3fc591385df5821681560ace1b27edbd84a0
xAlessandroo <alessandro.iozzino1@gmail.com>
2023-10-03 17:27:38 +0200

    Added alexID

---
+++ alexID.js
@@ -0,0 +1,12 @@
+let alexID = {
+    "name" : "Alessandro",
+    "surname" : "Weng",
+    "age" : 23,
+    "city" : "Naples",
+    "hobby" : "videogames",
+    "favoriteFood" : "",
+    "favoriteVideoGame" : "",
+    "favoriteFilm" : "",
+    "favoriteBook" : "",
+    "petName" : "Kiki"
+}
