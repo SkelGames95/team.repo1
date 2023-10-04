601360ed65639dc82162a6d8d9fbcd62a4f00ed0
Marco Polenta <55264003+SkelGames95@users.noreply.github.com>
2023-10-04 16:08:36 +0200

    Update team.js

--- team.js
+++ team.js
@@ -1,5 +1,5 @@
 let team = [
     {name:"Marco", surname:"Polenta", age:28, city:"San Severino Marche", hobby:"Videogames", favoriteFood:"Meat", favoriteVideoGame:"Cyberpunk2077", favoriteFilm:"Inception", favoriteBook:"Neuromancer", petName:"Kurama"},
     {name:"Giuseppe", surname:"Crimi", age:23, city:"Castelvetrano", hobby:"Videogames", favoriteFood:"Ravioli", favoriteVideoGame:"Point Blank", favoriteFilm:"Apocalypse Now", favoriteBook:"Dictionary", petName:"Sharon"},
-    {name : "Alessandro", surname : "Weng", age : 23, city : "Naples", hobby : "butterfly knife", favoriteFood : "Pizza", favoriteVideoGame : "", favoriteFilm : "", favoriteBook : "", petName : "Kiki"}
+    {name:"Alessandro", surname:"Weng", age:23, city:"Naples", hobby:"Butterfly Knife", favoriteFood:"Pizza", favoriteVideoGame:"Shooters", favoriteFilm:"", favoriteBook:"", petName:"Kiki"}
 ]
