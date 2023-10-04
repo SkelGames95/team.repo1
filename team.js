4001781f0bcb6334f76d546dbf0f1a1812726426
Marco.Polenta <polentamarco.dev@gmail.com>
2023-10-04 15:42:53 +0200

    personal object added

--- team.js
+++ team.js
@@ -1,2 +1,4 @@
 
-let team = []
+let team = [{
+name:"Marco", surname:"Polenta", age:"28", city:"San Severino Marche", hobby:"Videogames", favoriteFood:"Meat", favoriteVideoGame:"Cyberpunk2077", favoriteFilm:"Inception", favoriteBook:"Neuromancer", petName:"Kurama"
+}]
