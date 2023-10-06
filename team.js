33c0e32ba58f6241fbc20fbe281ea291cbaba8de
gabrieleprovenzano <146100399+gabrieleprovenzano@users.noreply.github.com>
2023-10-06 17:09:26 +0200

    Add files via upload

--- team.js
+++ team.js
@@ -3,5 +3,6 @@ let team = [
     {name:"Giuseppe", surname:"Crimi", age:23, city:"Castelvetrano", hobby:"Videogames", favoriteFood:"Ravioli", favoriteVideoGame:"Point Blank", favoriteFilm:"Apocalypse Now", favoriteBook:"Dictionary", petName:"Sharon"},
     {name:"Alessandro", surname:"Weng", age:23, city:"Naples", hobby:"Butterfly Knife", favoriteFood:"Pizza", favoriteVideoGame:"Shooters", favoriteFilm:"", favoriteBook:"", petName:"Kiki"},
     {name:"Ugo Davide", surname:"Vanacore", age:23, city:"Milano", hobby:"Reading", favoriteFood:"Pasta", favoriteVideoGame:"DragonBall", favoriteFilm:"V for Vendetta", favoriteBook:"Due candele per il diavolo", petName:"Ade"},
-    {name:"Federico", surname:"Puricella", age:25, city:"Torino", hobby:"Football", favoriteFood:"Pizza", favoriteVideoGame:"CallOfDuty", favoriteFilm:"", favoriteBook:"", petName:""}
+    {name:"Federico", surname:"Puricella", age:25, city:"Torino", hobby:"Football", favoriteFood:"Pizza", favoriteVideoGame:"CallOfDuty", favoriteFilm:"", favoriteBook:"", petName:""},
+    {name:"Gabriele", surname:"Provenzano", age:22, city:"Palermo", hobby:"Music", favoriteFood:"Pasta", favoriteVideoGame:"AssassinsCreedSaga", favoriteFilm:"StarWarsSaga", favoriteBook:"LaScienzaDiInterstellar", petName:""},
 ]
