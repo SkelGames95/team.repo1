d10f645b2ff9a10c3b598d41ee867236bc43f406
Marco.Polenta <polentamarco.dev@gmail.com>
2023-10-08 04:32:28 +0200

    Marco added LOL Function

--- team.js
+++ team.js
@@ -2,7 +2,27 @@ const team = [
     {name:"Marco", surname:"Polenta", age:28, city:"San Severino Marche", hobby:"Videogames", favoriteFood:"Meat", favoriteVideoGame:"Cyberpunk2077", favoriteFilm:"Inception", favoriteBook:"Neuromancer", petName:"Kurama"},
     {name:"Giuseppe", surname:"Crimi", age:23, city:"Castelvetrano", hobby:"Videogames", favoriteFood:"Ravioli", favoriteVideoGame:"Point Blank", favoriteFilm:"Apocalypse Now", favoriteBook:"Dictionary", petName:"Sharon"},
     {name:"Alessandro", surname:"Weng", age:23, city:"Naples", hobby:"Butterfly Knife", favoriteFood:"Pizza", favoriteVideoGame:"Shooters", favoriteFilm:"", favoriteBook:"", petName:"Kiki"},
-    {name:"Ugo Davide", surname:"Vanacore", age:23, city:"Milano", hobby:"Reading", favoriteFood:"Pasta", favoriteVideoGame:"DragonBall", favoriteFilm:"V for Vendetta", favoriteBook:"Due candele per il diavolo", petName:"Ade"},
+    {name:"Ugo Davide", surname:"Vanacore", age:29, city:"Milano", hobby:"Reading", favoriteFood:"Pasta", favoriteVideoGame:"DragonBall", favoriteFilm:"V for Vendetta", favoriteBook:"Due candele per il diavolo", petName:"Ade"},
     {name:"Federico", surname:"Puricella", age:25, city:"Torino", hobby:"Football", favoriteFood:"Pizza", favoriteVideoGame:"CallOfDuty", favoriteFilm:"", favoriteBook:"", petName:""},
-    {name:"Gabriele", surname:"Provenzano", age:22, city:"Palermo", hobby:"Music", favoriteFood:"Pasta", favoriteVideoGame:"Assassins Creed Saga", favoriteFilm:"Star Wars Saga", favoriteBook:"La Scienza Di Interstellar", petName:""}
+    {name:"Gabriele", surname:"Provenzano", age:22, city:"Palermo", hobby:"Music", favoriteFood:"Pasta", favoriteVideoGame:"Assassins Creed Saga", favoriteFilm:"Star Wars Saga", favoriteBook:"La Scienza Di Interstellar", petName:""},
 ]
+
+function players(arr){
+    let lolPlayers = [];
+        for (let i = 0 ; i < arr.length; i++){
+            let lolString = (arr[i].name + " " + arr[i].surname + " loves play LOL.");
+            if (arr[i].favoriteVideoGame === "League of Legends" || arr[i].favoriteVideoGame === "LOL"){
+                lolPlayers.push(lolString);
+        }
+    }
+    return lolPlayers;
+    
+}
+
+if (players(team).length > 0){
+    console.log(players(team));
+} else {
+    console.log("No one likes to play LOL");
+}
+
+console.log("--------------------------");
