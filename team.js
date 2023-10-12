fb2df045c860854166cf4d9e9259e05969ff330b
Federico <puricellafederico@gmail.com>
2023-10-12 16:02:31 +0200

    lavoro federico

--- team.js
+++ team.js
@@ -72,3 +72,7 @@ function hasSameName(names) {
   hasSameName(team);
 
 console.log("------------------------------------------------");
+
+function middleAge(array) {     let sum = 0;     for (let i = 0; i < array.length; i++) {         sum += array[i].age;     }     console.log(sum / array.length); }
+
+console.log("-----------------------------------------------");
